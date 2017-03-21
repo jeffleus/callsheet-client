311adee118314ef7a98ace09971a3e88c99d0e5a
Jeff Leininger <jeffleus@gmail.com>
2017-03-21 16:56:32 -0700

    init ionic project for callsheet project
    this is the ionic project that will provide a UI client to the
    callsheet backend that is a set of AWS microservices using
    API/Lambda/RDS.

---
+++ www/js/controllers.js
@@ -0,0 +1,86 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, AuthSvc) {
+    
+    $scope.authSvc = AuthSvc;
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  // Form data for the login modal
+  $scope.loginData = { username:"jeffleus-cs1", password:"GoBruins2017" };
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {      
+      if (AuthSvc.isAuthenticated()) {
+          return AuthSvc.logout();
+      } else {
+          $scope.modal.show();          
+      }
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+    
+    AuthSvc.getUser().then(function(token) {
+        if (token != null) {
+            console.log(token);
+            return token;
+        } else {
+            return AuthSvc.login($scope.loginData)
+            .then(function(token) {
+                console.log('back to the controller...');
+                $scope.modal.hide();
+                return token;
+            }).catch(function(err) {
+                console.error('error bubbled to the controller...');
+            });            
+        }
+    });
+    
+
+//    // Simulate a login delay. Remove this and replace with your login
+//    // code if using a login system
+//    $timeout(function() {
+//      $scope.closeLogin();
+//    }, 1000);
+  };
+})
+
+.controller('PlaylistsCtrl', function($scope, $http, AuthSvc) {
+    AuthSvc.getUser().then(function(token) {
+        var options = {
+            headers: {
+                'Authorization': token
+            }
+        };
+        return $http.get('http://localhost:8100/employees', options)
+        .then(function(results) {
+            console.log(results.data.message);
+            $scope.employees = results.data.employees;
+            return results.data.message;
+        });
+    }).catch(function(err) {
+        console.error(err);
+    });
+})
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+});
